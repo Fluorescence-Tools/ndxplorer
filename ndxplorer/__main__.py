7dcf2f2101dc32eb8899a470a1cd4b3b67942b38
tpeulen <thomas.peulen@posteo.de>
2022-12-05 14:47:21 +0100

    Remove deprecated code

--- ndxplorer/__main__.py
+++ ndxplorer/__main__.py
@@ -1,13 +1,4 @@
 import sys
-import sip
-sip.setapi('QDate', 2)
-sip.setapi('QDateTime', 2)
-sip.setapi('QString', 2)
-sip.setapi('QTextStream', 2)
-sip.setapi('QTime', 2)
-sip.setapi('QUrl', 2)
-sip.setapi('QVariant', 2)
-
 from qtpy.QtWidgets import QApplication
 from ndxplorer import plot_main
 
