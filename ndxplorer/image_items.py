c612415524fbd5badc9c64dcb9edaf57e908faf5
tpeulen <thomas.peulen@posteo.de>
2025-07-13 21:14:54 +0200

    Add `FixedImageItem` class to resolve float to int conversion issue in `ndxplorer` visualization

---
+++ ndxplorer/image_items.py
@@ -0,0 +1,33 @@
+"""
+Custom image item classes for ndxplorer visualization.
+"""
+
+import numpy as np
+import guiqwt.image
+from qtpy.QtGui import QImage
+
+# Custom ImageItem class that fixes the float to int conversion issue
+class FixedImageItem(guiqwt.image.ImageItem):
+    def draw(self, painter, xMap, yMap, canvasRect):
+        x1, y1, x2, y2 = canvasRect.getCoords()
+        i1, i2 = xMap.invTransform(x1), xMap.invTransform(x2)
+        j1, j2 = yMap.invTransform(y1), yMap.invTransform(y2)
+
+        xl, yt, xr, yb = self.boundingRect().getCoords()
+        dest = (
+            xMap.transform(xl),
+            yMap.transform(yt),
+            xMap.transform(xr) + 1,
+            yMap.transform(yb) + 1,
+        )
+
+        # Convert float to int for W and H
+        W = int(canvasRect.right())
+        H = int(canvasRect.bottom())
+        if self._offscreen.shape != (H, W):
+            self._offscreen = np.empty((H, W), np.uint32)
+            self._image = QImage(self._offscreen, W, H, QImage.Format_ARGB32)
+            self._image.ndarray = self._offscreen
+            self.notify_new_offscreen()
+        self.draw_image(painter, canvasRect, (i1, j1, i2, j2), dest, xMap, yMap)
+        self.draw_border(painter, xMap, yMap, canvasRect)
